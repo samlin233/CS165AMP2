186f89443104ef06181a084d4d279036d9e1467c
samlin233 <senlin@umail.ucsb.edu>
2020-06-07 08:01:41 -0700

    #

--- CS165Amp2.cpp
+++ CS165Amp2.cpp
@@ -227,11 +227,11 @@ long int score(position p, int calcopp,int size){
        }
     }
     if (win5 >= 1)
-        return 1500;//贏5
+        return 15000;//贏5
     if (huo4 >= 1 || si4 >= 2 || (si4 >= 1 && huo3 >= 1))
-        return 140;//活4 双死4 死4活3
+        return 1400;//活4 双死4 死4活3
     if (huo3 >= 2)
-        return 13;//双活3
+        return 1300;//双活3
     if (si3 >= 1 && huo3 >= 1)
         return 12;//死3活3
     if (si4 >= 1)
